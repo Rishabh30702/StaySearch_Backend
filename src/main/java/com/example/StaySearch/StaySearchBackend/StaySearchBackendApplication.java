18f59de14682e372529d3abfce5409530147d7a3
amchangama <rishabh.singh@valliento.tech>
2025-05-17 16:28:15 +0530

    FIXED: Timezone issue on the server

--- src/main/java/com/example/StaySearch/StaySearchBackend/StaySearchBackendApplication.java
+++ src/main/java/com/example/StaySearch/StaySearchBackend/StaySearchBackendApplication.java
@@ -1,9 +1,12 @@
 package com.example.StaySearch.StaySearchBackend;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableScheduling
@@ -13,5 +16,9 @@ public class StaySearchBackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(StaySearchBackendApplication.class, args);
 	}
-
+	@PostConstruct
+	public void init(){
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
+		System.out.println("Spring Boot Application TimeZone set to Asia/Kolkata");
+	}
 }
